0c2078265a7924d08cd4931f0718d19da4385ff5
LeoAndriolo <leonardoandriolo.la@gmail.com>
2024-01-15 21:49:52 +0000

    Update some objetcts and character physics

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function preload() {
 }
 
 function create() {
-  var fundo = this.physics.add.image(300,2400, 'fundo').setScale(4).setVelocityY(-400);// Cria o elemento de fundo
+  var fundo = this.physics.add
+    .image(300, 2000, 'fundo')
+    .setScale(4)
+    .setVelocityY(-400);// Cria o elemento de fundo
   //fundo.create(300, 2400, 'fundo').setScale(4).setGravityY(-300);
   // this.add.image(300, 2400, 'fundo').setScale(4);//cria o fundo e o posiciona no centro da tela
   // var chao = this.physics.add.staticGroup();//cria um grupo de chão
@@ -32,86 +35,81 @@ function create() {
   // platform.create(50, 150, 'obstaculo');//cria a plataforma
   // platform.create(550, 250, 'obstaculo');//cria a plataforma
   meteoro.create(300, 400).setGravity(0, -300).setScale(0.75);//cria a plataforma
-  
-  var personagem = this.physics.add.image(300, 80, 'personagem').setScale(0.5);//cria o personagem
+
+  var personagem = this.physics.add
+    .image(300, 80, 'personagem')
+    .setScale(0.5)
+    .setDrag(300, 300)
+    .setMaxVelocity(600, 600);//cria o personagem
   // var personagem = this.physic.add.sprite(100, 330, 'personagem');//posição inicial do personagem
   personagem.setCollideWorldBounds(true);//personagem não sair para fora da plataforma
   //personagem.setGravity(0,3000); //personagem sofre gravidade
 
-  // this.anims.create({//cria o personagem parado
-  //   key: 'parado',
-  //   frames: [{ key: 'personagem', frame: 4 }],//onde está o personagem parado na imagem
-  // });
-
-  // this.anims.create({//cria o personagem andando para a esquerda
-  //   key: 'esquerda',
-  //   frames: this.anims.generateFrameNumbers('personagem', { start: 0, end: 3 }),//frame que começa e acaba
-  //   frameRate: 10,//velocidade que muda o sprite
-  //   repeat: -1//??
-  // });
-
-  // this.anims.create({//cria o personagem andando para a direita
-  //   key: 'direita',
-  //   frames: this.anims.generateFrameNumbers('personagem', { start: 5, end: 8 }),//frame que começa e acaba
-  //   frameRate: 10,//velocidade que muda o sprite
-  //   repeat: -1//??
-  // });
   // this.chao = chao;//atribui o chão a uma variável
   this.fundo = fundo;//atribui o fundo a uma variável
   this.meteoro = meteoro;//atribui o meteoro a uma variável
   this.personagem = personagem;//atribui o personagem a uma variavel
   this.ovni = ovni;//atribui a plataforma a uma variavel
-  this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);//seta para cima
+  //this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);//seta para cima
   this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);//seta para esquerda
-  this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);//seta para baixo
+ // this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);//seta para baixo
   this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);//seta para direita
   this.physics.add.collider(this.personagem, this.ovni);
   this.physics.add.collider(this.personagem, this.meteoro);
-  this.physics.add.collider(this.ovni, this.meteoro);
-  // this.physics.add.collider(this.personagem, this.chao);
+  
+  var vida = 3;
+  this.add.text(16, 16, 'Vida: ' + vida);
+  this.vida = vida;
+  
+  // console.log('Vida: '+this.vida);
+  // this.physics.add.overlap(this.personagem, this.meteoro, vida--, null, this);
+  // console.log('Vida depois: '+this.vida);
 }
 
-function update() { 
+function update() {
+  //var vida = this.vida;
   var fundo = this.fundo;
   let cursors = this.input.keyboard.createCursorKeys();//cria as setas do teclado
   var personagem = this.personagem;//atribui o personagem a uma variavel
-  
+
   if (cursors.left.isDown || this.a.isDown) { //se a seta esquerda ou a for pressionada
-    personagem.setVelocityX(-160);//personagem anda para a esquerda, com velocidade de 160 pixels por segundo
+    personagem.setVelocityX(-300);//personagem anda para a esquerda, com velocidade de 160 pixels por segundo
     // personagem.anims.play('esquerda', true);//ativa animaçao de andar para a esquerda	
   } else if (cursors.right.isDown || this.d.isDown) { // senão se a seta direita ou d for pressionada
-    personagem.setVelocityX(160);//personagem anda para a direita, com velocidade de 160 pixels por segundo
+    personagem.setVelocityX(300);//personagem anda para a direita, com velocidade de 160 pixels por segundo
     // personagem.anims.play('direita', true);//ativa animaçao de andar para a direita
-  } else { //senão 
-    personagem.setVelocityX(0);//personagem parado
-    // personagem.anims.play('parado');//ativa animaçao de parado
   }
-  if (cursors.up.isDown || this.w.isDown) { //senão se a seta cima ou w for pressionada
-    personagem.setVelocityY(-160);//personagem anda para cima, com velocidade de 160 pixels por segundo
-  } else if (cursors.down.isDown || this.s.isDown) { //senão se a seta baixo ou s for pressionada
-    personagem.setVelocityY(160);//personagem anda para baixo, com velocidade de 160 pixels por segundo
-  } else {
-    personagem.setVelocityY(0);//personagem parado
-  }
+  // else { //senão 
+  //   personagem.setVelocityX(0);//personagem parado
+  //   // personagem.anims.play('parado');//ativa animaçao de parado
+  // }
+  // if (cursors.up.isDown || this.w.isDown) { //senão se a seta cima ou w for pressionada
+  //   personagem.setVelocityY(-160);//personagem anda para cima, com velocidade de 160 pixels por segundo
+  // } else if (cursors.down.isDown || this.s.isDown) { //senão se a seta baixo ou s for pressionada
+  //   personagem.setVelocityY(160);//personagem anda para baixo, com velocidade de 160 pixels por segundo
+  // } else {
+  //   personagem.setVelocityY(0);//personagem parado
+  // }
   //console.log(fundo.y);
-  if (fundo.y < -2100) {
-    fundo.y = 2400;
+  if (fundo.y < -1600) {
+    fundo.y = 2000;
   }
+  
 }
 
 const config = {
   type: Phaser.AUTO, // Canva ou WebGL
   width: 600,//largura
-  height: 400,//altura
+  height: 800,//altura
   backgroundColor: '#f9f9f9',//cor de fundo
-  autoCenter: Phaser.Scale.CENTER_BOTH,//centraliza o jogo
+  autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,//centraliza o jogo
   physics: { //fisica
     default: 'arcade', //tipo de fisica
     arcade: {
       gravity: {
         y: 0 //gravidade
       },
-      debug: false 
+      debug: false
     }
   },
   scene: {
