49622c0a84850a303e61df8e873ca2b84a5b18a3
LeoAndriolo <leonardoandriolo.la@gmail.com>
2024-01-18 19:51:36 +0000

    Upload images and restart logic

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function preload() {
+  this.load.image("front", "assets/front.jpg");
   this.load.image("fundo", "assets/fundo.png"); // Carrega a imagem de fundo
   this.load.image("ovni", "assets/ovni.png"); // Carrega a imagem do obstáculo
   this.load.image("meteoro", "assets/meteoro.png"); // Carrega a imagem do meteoro
   this.load.image("personagem", "assets/astronauta.png"); // Carrega a imagem do personagem
   this.load.image("red", "assets/red.png"); // Carrega o atlas de partículas
+  this.load.audio("theme", "assets/audio/DavidKBD_Nebula Run.ogg");
 }
 
 function create() {
@@ -11,6 +13,10 @@ function create() {
     .image(300, 2000, "fundo")
     .setScale(4)
     .setVelocityY(-400); // Cria o elemento de fundo
+
+  // Adiciona o som de fundo
+  const music = this.sound.add("theme", { volume: 0.1 });
+  music.play();
 
   // Adiciona o atlas de partículas ao personagem
   this.flame = this.add.particles(0, 0, "red", {
@@ -33,12 +39,12 @@ function create() {
   ovni.create(500, 250).setGravity(0, -300).setScale(0.75); //cria o ovni
 
   var meteoro = this.physics.add
-    .image(300, 500, "meteoro")
+    .image(100, 600, "meteoro")
     .setScale(0.75)
     .setSize(160, 5)
     .setOffset(0, 0)
     .setGravity(0, -300)
-    .setMaxVelocity(0, 300);; // Cria o elemento de meteoro
+    .setMaxVelocity(0, 600); // Cria o elemento de meteoro
 
   // var meteoro = this.physics.add.group(
   //   {
@@ -55,9 +61,7 @@ function create() {
     .setOffset(0, 160)
     .setDrag(300, 300)
     .setMaxVelocity(600, 600); //cria o personagem
-  // var personagem = this.physic.add.sprite(100, 330, 'personagem');//posição inicial do personagem
   personagem.setCollideWorldBounds(true); //personagem não sair para fora da plataforma
-  //personagem.setGravity(0,3000); //personagem sofre gravidade
 
   // this.chao = chao;//atribui o chão a uma variável
   this.fundo = fundo; //atribui o fundo a uma variável
@@ -68,6 +72,7 @@ function create() {
   this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A); //seta para esquerda
   // this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);//seta para baixo
   this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D); //seta para direita
+  
   this.physics.add.collider(this.personagem, this.ovni);
   // this.physics.add.collider(this.personagem, this.meteoro);
 
@@ -80,26 +85,46 @@ function create() {
     this.personagem,
     this.reduceLife,
     null,
-    this
+    this,
   );
 
   this.flame.startFollow(personagem); // Adiciona rastro rosa ao personagem
+
+  // Reset Button
+  const buttonBox = this.add.rectangle(this.sys.scale.width / 2, this.sys.scale.height - 300, 290, 50, 0x000000, 1);
+  buttonBox.setInteractive();
+  const buttonText = this.add.text(this.sys.scale.width / 2, this.sys.scale.height - 300, "Restart").setOrigin(0.5);
+
+  buttonBox.on('pointerdown', () => {
+      this.scene.restart();
+  });
+  
+  // Hover button properties
+  buttonBox.on('pointerover', () => {
+      buttonBox.setFillStyle(0x222222, 1);
+      this.input.setDefaultCursor('pointer');
+  });
+
+  buttonBox.on('pointerout', () => {
+      buttonBox.setFillStyle(0x000000, 1);
+      this.input.setDefaultCursor('default');
+  });
+
+  // Hide Button
+  buttonBox.setAlpha(0);
+  buttonText.setAlpha(0);
+
+  this.buttonBox = buttonBox;
+  this.buttonText = buttonText;
 }
 
 function update() {
-  // reduceLife(){
-  //   this.vida--;
-  //   this.add.text(16, 16, 'Vida: ' + this.vida);
-  //   // if(this.vida <= 0){
-  //   //   this.scene.start('GameOver');
-  //   // }
-  // }
-
-  //var vida = this.vida;
   var fundo = this.fundo;
   let cursors = this.input.keyboard.createCursorKeys(); //cria as setas do teclado
   var personagem = this.personagem; //atribui o personagem a uma variavel
   var meteoro = this.meteoro; //atribui o meteoro a uma variavel
+  var buttonBox = this.buttonBox;
+  var buttonText = this.buttonText;
   
   if (cursors.left.isDown || this.a.isDown) {
     //se a seta esquerda ou a for pressionada
@@ -108,7 +133,7 @@ function update() {
     // senão se a seta direita ou d for pressionada
     personagem.setVelocityX(300); //personagem move para a direita, com velocidade de 300 pixels por segundo
   }
-
+  
   //console.log(fundo.y);
   if (fundo.y < -1600) {
     fundo.y = 2000;
@@ -118,17 +143,28 @@ function update() {
     meteoro.setY(850); // meteoro volta para baixo
     meteoro.setX(Phaser.Math.Between(50, 550)); // posição aleatória do meteoro
   }
+
+  if (this.vida <= 0) {
+    this.add
+      .text(100, 400, "Game Over")
+      .setFontFamily("Poppins")
+      .setFontSize(64)
+      .setColor("#ff0000");
+    this.physics.pause();
+    this.sound
+    this.sound.pauseAll();
+    buttonBox.setAlpha(1);
+    buttonText.setAlpha(1);
+    
+  }
+  
 }
 
 function reduceLife() {
   this.vida--;
   this.textVidas.setText("Vida: " + this.vida);
   this.personagem.setTint(0xff00ff, 0xff0000);
-  // this.personagem.clearTint();
-  if(this.vida <= 0){
-    this.textVidas.setText("Vida: Game Over");
-    this.scene.pause();
-  }
+
 }
 
 const config = {
@@ -144,16 +180,16 @@ const config = {
       gravity: {
         y: 0, //gravidade
       },
-      debug: true,
+      debug: false,
     },
   },
   scene: {
     extend: {
-      reduceLife: reduceLife, // <- function added to the scene
+      reduceLife: reduceLife// <- function added to the scene
     },
     preload: preload,
     create: create,
-    update: update,
+    update: update
   },
 };
 
